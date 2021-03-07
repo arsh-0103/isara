722a2127da352d8a7332802c2071b8c7971f2ddb
Arai <arsh.design0103@gmail.com>
2021-03-07 17:13:44 +0900

    add:html css js / wrapper-20

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
 $(function(){
-
-$("#scroll-btn").click(function () {
+    $("#scroll-btn").click(function () {
     $('html, body').animate({ 'scrollTop': 0 }, 500,'swing');
+});
+
+    $('.js-accordion-title').on('click', function () {
+    /*クリックでコンテンツを開閉*/
+    $(this).next().slideToggle(200);
+    /*矢印の向きを変更*/
+    $(this).toggleClass('open', 200);
+  });
+
+
+  $('.js-question-title').on('click',function(){
+    $('.answer').slideToggle(200);
+    $(this).toggleClass('open', 200); 
   });
 
 });
+

7384133bb7d355bfbc78a674380fe90e83f48525
Arai <arsh.design0103@gmail.com>
2021-03-08 16:02:23 +0900

    add:js scroll/fadeIn

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 $(function(){
   
+
+    $(window).scroll(function(){
+        if($(this).scrollTop() > 100) {
+            $('#scroll-btn').fadeIn(100);
+        }
+        else {
+            $('#scroll-btn').fadeOut(100);
+        }
+    });
+
     $("#scroll-btn").click(function () {
     $('html, body').animate({ 'scrollTop': 0 }, 500,'swing');
    });
